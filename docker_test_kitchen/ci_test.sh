4b2f9a468161f99cab9a7c2b62e4e454252a9697
Clif Reeder <clif@voxmedia.com>
2016-05-24 13:31:49 -0400

    Add rubocop and foodcritic

--- docker_test_kitchen/ci_test.sh
+++ docker_test_kitchen/ci_test.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 
 ssh-keygen -y -f /root/.ssh/id_rsa > /root/.ssh/id_rsa.pub
 bundle install --path vendor/bundle
+
+bundle exec foodcritic ./
+bundle exec rubocop
 bundle exec kitchen test
