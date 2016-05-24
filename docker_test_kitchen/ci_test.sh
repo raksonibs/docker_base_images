c498e2bbe59bfaeed402e7bfe31a214e6284960e
Clif Reeder <clif@voxmedia.com>
2016-05-24 14:00:08 -0400

    Make food critic exit with error

--- docker_test_kitchen/ci_test.sh
+++ docker_test_kitchen/ci_test.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 ssh-keygen -y -f /root/.ssh/id_rsa > /root/.ssh/id_rsa.pub
 bundle install --path vendor/bundle
 
-bundle exec foodcritic ./
+bundle exec foodcritic --epic-fail any ./
 bundle exec rubocop
 bundle exec kitchen test
