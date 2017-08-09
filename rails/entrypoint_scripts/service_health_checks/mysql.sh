6940b9a5d49628a83b64c2aa334a7f1670b01472
Ben Lee <ben.lee@voxmedia.com>
2017-08-09 12:28:30 -0400

    it's DATABASE_HOST not MYSQL_HOST

--- rails/entrypoint_scripts/service_health_checks/mysql.sh
+++ rails/entrypoint_scripts/service_health_checks/mysql.sh
@@ -1,8 +1,8 @@
-# if MYSQL_HOST is present, make sure mysql is alve before proceeding
-if [ ! -z "${MYSQL_HOST}" ]; then
+# if DATABASE_HOST is present, make sure mysql is alve before proceeding
+if [ ! -z "${DATABASE_HOST}" ]; then
   MYSQL_UP=false
   for i in $(seq 1 10); do
-    if mysqladmin ping -h ${MYSQL_HOST} --silent > /dev/null; then
+    if mysqladmin ping -h ${DATABASE_HOST} --silent > /dev/null; then
       MYSQL_UP=true
       break
     else
@@ -11,7 +11,7 @@ if [ ! -z "${MYSQL_HOST}" ]; then
   done
 
   if [ $MYSQL_UP = "false" ]; then
-    echo "Unable to connect to database. Please make sure your ${MYSQL_HOST} container is running and healthy."
+    echo "Unable to connect to database. Please make sure your ${DATABASE_HOST} container is running and healthy."
     exit 1
   fi
 fi
