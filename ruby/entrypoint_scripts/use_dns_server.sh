f3571335f93241ce9c76195ee6b998255f61c331
Ben Lee <ben.lee@voxmedia.com>
2017-10-27 17:52:38 -0400

    only rewrite dns servers if the specified server ip is found

--- ruby/entrypoint_scripts/use_dns_server.sh
+++ ruby/entrypoint_scripts/use_dns_server.sh
@@ -1,3 +1,5 @@
 DNS_SERVER_HOST=${1:-dns}
 DNS_SERVER_IP=$(getent hosts $DNS_SERVER_HOST | awk '{ print $1 }')
-echo "nameserver $DNS_SERVER_IP" > /etc/resolv.conf
+if [[ ! -z $DNS_SERVER_IP ]]; then
+  echo "nameserver $DNS_SERVER_IP" > /etc/resolv.conf
+fi
