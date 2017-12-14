11637d1a3ed23ccade39a89f33cb532d7c6d84c2
Keaty Gross <keaty.gross@voxmedia.com>
2017-12-14 13:48:18 -0500

    Use value of ELASTICSEARCH_HOST env var as ES hostname in healthcheck
    * and do not run healthcheck if ELASTICSEARCH_HOST is not set

--- ruby/entrypoint_scripts/service_health_checks/elasticsearch.sh
+++ ruby/entrypoint_scripts/service_health_checks/elasticsearch.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # make sure ES is alive before proceeding
-PORT=${1:-9200}
-ES_UP=false
-for i in {1..10}; do
-  status=$(wget -qO- http://elastic:$PORT/_cat/health?h=status | tr -d [:space:])
-  if [ "$status" = "yellow" ] || [ "$status" = "green" ]; then
-    ES_UP=true
-    break
-  else
-    WAIT=$(($i>5?5:$i))
-    echo "Waiting $WAIT seconds for Elasticsearch to start..."
-    sleep $WAIT
-  fi
-done
+if [ ! -z "${ELASTICSEARCH_HOST}" ]; then
+  PORT=${1:-9200}
+  ES_UP=false
+  for i in {1..10}; do
+    status=$(wget -qO- http://$ELASTICSEARCH_HOST:$PORT/_cat/health?h=status | tr -d [:space:])
+    if [ "$status" = "yellow" ] || [ "$status" = "green" ]; then
+      ES_UP=true
+      break
+    else
+      WAIT=$(($i>5?5:$i))
+      echo "Waiting $WAIT seconds for Elasticsearch to start..."
+      sleep $WAIT
+    fi
+  done
 
-if [ $ES_UP = false ]; then
-  echo "Unable to connect to Elasticsearch. Please make sure your Elasticsearch container is running and healthy."
-  exit 1
+  if [ $ES_UP = false ]; then
+    echo "Unable to connect to Elasticsearch. Please make sure your Elasticsearch container is running and healthy."
+    exit 1
+  fi
 fi
