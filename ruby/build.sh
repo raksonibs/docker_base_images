a91f8d68df10d0b0dcec299cdba09917babb58af
Curtis <curtis.schiewek@gmail.com>
2018-01-05 17:06:53 -0500

    remove 2.5 for now

--- ruby/build.sh
+++ ruby/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 version=$(cat VERSION)
-ruby_versions=("2.2" "2.3" "2.4" "2.5")
+ruby_versions=("2.2" "2.3" "2.4")
 for ruby in ${ruby_versions[@]}
 do
 	tag="voxmedia/ruby:$ruby-$version"
 	docker build . --build-arg RUBY_VERSION=$ruby -t $tag
-	docker push $tag
+	#docker push $tag
 done
