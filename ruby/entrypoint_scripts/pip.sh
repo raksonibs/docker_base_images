70398e051dde13aa25a43592712a6155dea0ac64
Ben Lee <ben.lee@voxmedia.com>
2018-02-16 17:58:30 -0500

    even quiter pip output

--- ruby/entrypoint_scripts/pip.sh
+++ ruby/entrypoint_scripts/pip.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -euxo pipefail
-
 DEV_REQUIREMENTS_FILE=${DEV_REQUIREMENTS_FILE:-dev_requirements.txt}
 
 if [ "$PIP_ENV" = "development" ]; then
+  echo "Ensuring pip dev requirements are installed"
   pip install -q -r $DEV_REQUIREMENTS_FILE
 fi
