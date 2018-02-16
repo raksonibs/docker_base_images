e3ae1af95621449b17deab90fde41449a16fc6a5
Ben Lee <ben.lee@voxmedia.com>
2018-02-16 14:37:22 -0500

    oops, need to use env var here

--- ruby/entrypoint_scripts/pip.sh
+++ ruby/entrypoint_scripts/pip.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-DEV_REQUIREMENTS_FILE=${1:-dev_requirements.txt}
+DEV_REQUIREMENTS_FILE=${DEV_REQUIREMENTS_FILE:-dev_requirements.txt}
 
 if [ "$PIP_ENV" = "development" ]; then
   pip install -r $DEV_REQUIREMENTS_FILE
