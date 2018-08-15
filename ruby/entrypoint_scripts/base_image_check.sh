c6c552e0d637804dad55b982341cc4a9fe98ba8a
Eric Binmore <eric.binmore@voxmedia.com>
2018-08-15 18:15:52 -0400

    Use tmp folders and clean up mess -- better :-D

--- ruby/entrypoint_scripts/base_image_check.sh
+++ ruby/entrypoint_scripts/base_image_check.sh
@@ -3,19 +3,20 @@
 ## each separate version number must be less than 3 digit wide !
 function version { echo "$@" | awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
 
-# get the VERSION and CHANGELOG.md file from the docker_base_images repo
-(cd /tmp && \
-docker-ssh-exec git clone --quiet --depth=1 --bare git@github.com:voxmedia/docker_base_images.git TEMPDIRNAME123 && \
-mkdir -p ruby && \
-docker-ssh-exec git --bare --git-dir=TEMPDIRNAME123 --work-tree=./ checkout --quiet -f HEAD -- ruby/VERSION ruby/CHANGELOG.md && \
-rm -rf TEMPDIRNAME123) &> /tmp/base_image_check.log
+# get the VERSION and CHANGELOG.md file from the docker_base_images repo and store in a temp folder
+GITDIR=`mktemp -d`
+WORKDIR=`mktemp -d`
+
+(docker-ssh-exec git clone --quiet --depth=1 --bare git@github.com:voxmedia/docker_base_images.git $GITDIR && \
+docker-ssh-exec git --bare --git-dir=$GITDIR --work-tree=$WORKDIR checkout --quiet -f HEAD -- ruby/VERSION ruby/CHANGELOG.md) \
+&> /tmp/base_image_check.log
 
 if [ $? -gt 0 ] ; then
   # error getting version...
   echo "⚠️   Unable to retrieve the current base image information from the github repo.  ⚠️"
   echo "⚠️   Are you connected to the internet?                                           ⚠️"
 else
-  LATEST_VERSION=`cat /tmp/ruby/VERSION`
+  LATEST_VERSION=`cat $WORKDIR/ruby/VERSION`
   # get the current base image version -- $IMAGE_VERSION -- is added to the base image on build
   if [ "$(version "$LATEST_VERSION")" -gt "$(version "$IMAGE_VERSION")" ] ; then
     echo "🌩  There is a newer version of the ruby docker base image available.  🌩"
@@ -29,7 +30,7 @@ else
         break;
       fi
       echo "$line"
-    done < <(cat /tmp/ruby/CHANGELOG.md)
+    done < <(cat $WORKDIR/ruby/CHANGELOG.md)
     echo "..."
     echo "################################################################################"
     echo
@@ -38,3 +39,7 @@ else
     echo "The current base image ($IMAGE_VERSION) is up-to-date! 👍"
   fi
 fi
+
+# clean up any mess
+rm -rf $GITDIR
+rm -rf $WORKDIR
