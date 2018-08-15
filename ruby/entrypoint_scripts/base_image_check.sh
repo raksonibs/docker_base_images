4bb6f7cf863a380e9860fa65abc18f41f514de20
Eric Binmore <eric.binmore@voxmedia.com>
2018-08-15 17:28:54 -0400

    Use git to get VERSION and CHANGELOG.md at the same time

--- ruby/entrypoint_scripts/base_image_check.sh
+++ ruby/entrypoint_scripts/base_image_check.sh
@@ -4,36 +4,34 @@
 function version { echo "$@" | awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
 
 # get the VERSION and CHANGELOG.md file from the docker_base_images repo
-svn --quiet --force export https://github.com/voxmedia/docker_base_images/trunk/ruby/VERSION /tmp/VERSION
+(cd /tmp && \
+docker-ssh-exec git clone --quiet --depth=1 --bare git@github.com:voxmedia/docker_base_images.git TEMPDIRNAME123 && \
+mkdir -p ruby && \
+docker-ssh-exec git --bare --git-dir=TEMPDIRNAME123 --work-tree=./ checkout --quiet -f HEAD -- ruby/VERSION ruby/CHANGELOG.md && \
+rm -rf TEMPDIRNAME123) &> /tmp/base_image_check.log
+
 if [ $? -gt 0 ] ; then
   # error getting version...
-  echo "⚠️   Unable to retrieve the current VERSION for the base image from the github repo.  ⚠️"
-  echo "⚠️   Are you connected to the internet?                                               ⚠️"
+  echo "⚠️   Unable to retrieve the current base image information from the github repo.  ⚠️"
+  echo "⚠️   Are you connected to the internet?                                           ⚠️"
 else
-  LATEST_VERSION=`cat /tmp/VERSION`
+  LATEST_VERSION=`cat /tmp/ruby/VERSION`
   # get the current base image version -- $IMAGE_VERSION -- is added to the base image on build
   if [ "$(version "$LATEST_VERSION")" -gt "$(version "$IMAGE_VERSION")" ] ; then
     echo "🌩  There is a newer version of the ruby docker base image available.  🌩"
     echo "🌩  Please update your docker image to use the latest version.         🌩"
     echo
-
-    svn --quiet --force export https://github.com/voxmedia/docker_base_images/trunk/ruby/CHANGELOG.md /tmp/CHANGELOG.md
-    if [ $? -gt 0 ] ; then
-      # error getting version...
-      echo "Unable to retrieve the CHANGELOG.md for the base image from the github repo."
-    else
-      echo "################################################################################"
-      # now output the CHANGELOG.md
-      # https://stackoverflow.com/a/10929511/1571834
-      while IFS='' read -r line || [[ -n "$line" ]]; do
-        if [[ $line = *"$IMAGE_VERSION"* ]]; then
-          break;
-        fi
-        echo "$line"
-      done < <(cat /tmp/CHANGELOG.md)
-      echo "..."
-      echo "################################################################################"
-    fi
+    echo "################################################################################"
+    # now output the CHANGELOG.md
+    # https://stackoverflow.com/a/10929511/1571834
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+      if [[ $line = *"$IMAGE_VERSION"* ]]; then
+        break;
+      fi
+      echo "$line"
+    done < <(cat /tmp/ruby/CHANGELOG.md)
+    echo "..."
+    echo "################################################################################"
     echo
     echo "To see the full CHANGELOG, please visit https://github.com/voxmedia/docker_base_images/ruby/CHANGELOG.md"
   else
