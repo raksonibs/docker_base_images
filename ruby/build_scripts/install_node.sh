a3d01d96b374082a9d91673e424d30432ad201d7
okor <jason.ormand1@gmail.com>
2018-08-29 17:58:48 -0400

    add retry logic for fetch keys, update keys

--- ruby/build_scripts/install_node.sh
+++ ruby/build_scripts/install_node.sh
@@ -4,24 +4,44 @@ set -ex
 NODE_VERSION=${1:-8.11.3}
 
 # gpg keys listed at https://github.com/nodejs/node#release-team
-for key in \
-  114F43EE0176B71C7BC219DD50A3051F888C628D \
-  56730D5401028683275BD23C23EFEFE93C4CFFFE \
-  71DCFD284A79C3B38668286BC97EC7A07EDE3FC1 \
-  77984A986EBC2AA786BC0F66B01FBB92821C587A \
-  7937DFD2AB06298B2293C3187D33FF9D0246406D \
-  93C7E9E91B49E432C2F75674B0A78B0A6C481CF6 \
-  94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
-  9554F04D7259F04124DE6B476D5A82AC7E37093B \
-  B9AE9905FFD7803F25714661B63B535A4C206CA9 \
-  C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
-  DD8F2338BAE7501E3DD5AC78C273792F7D83545D \
-  FD3A5288F042B6850C66B31F09FE44734EB7990E \
-; do
-    gpg --keyserver pool.sks-keyservers.net --recv-keys "$key" || \
-    gpg --keyserver keyserver.pgp.com --recv-keys "$key" || \
-    gpg --keyserver pgp.mit.edu --recv-keys "$key" ; \
+# Updated: Aug 29th, 2018
+KEYS="94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
+      B9AE9905FFD7803F25714661B63B535A4C206CA9 \
+      77984A986EBC2AA786BC0F66B01FBB92821C587A \
+      71DCFD284A79C3B38668286BC97EC7A07EDE3FC1 \
+      FD3A5288F042B6850C66B31F09FE44734EB7990E \
+      8FCCA13FEF1D0C2E91008E09770F7A9A5AE15600 \
+      C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
+      DD8F2338BAE7501E3DD5AC78C273792F7D83545D"
+
+SERVERS="pool.sks-keyservers.net \
+         keyserver.pgp.com \
+         pgp.mit.edu"
+
+function importKeys() {
+  for key in $KEYS; do
+    for server in $SERVERS; do
+      gpg --keyserver "hkp://$server" --recv-keys "$key" && break
+    done
+  done
+  return $?
+}
+
+retry=0
+maxRetries=5
+retryInterval=5
+until [ ${retry} -ge ${maxRetries} ]
+do
+  importKeys && break
+  retry=$[${retry}+1]
+  echo "Key import failed. Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+  sleep ${retryInterval}
 done
+
+if [ ${retry} -ge ${maxRetries} ]; then
+  echo "Failed to import keys after ${maxRetries} attempts!"
+  exit 1
+fi
 
 curl -SLO "https://nodejs.org/download/release/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz"
 
