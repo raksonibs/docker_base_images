31913dec225209f2a89cdfa0636bb9e564c97158
Jamie McCarthy <jamie.mccarthy@voxmedia.com>
2018-10-17 11:37:58 -0500

    A cleaner way to check empty dir
    Thanks @thenonsequitur

--- ruby/entrypoint_scripts/yarn.sh
+++ ruby/entrypoint_scripts/yarn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Verifying node modules are up-to-date with yarn"
-if [[ $((`ls ./node_modules|wc -l`)) == 0 ]] || ! yarn check --silent ; then
+if [ ! "$(ls -A ./node_modules)" ] || ! yarn check --silent ; then
   yarn install --silent --check-files --mutex file
 fi
