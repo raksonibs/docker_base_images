39fb44c68b7c0bf58764f873185425bb10ff0dcb
Jamie McCarthy <jamie.mccarthy@voxmedia.com>
2018-10-17 10:30:19 -0500

    yarn commands --silent
    This removes various unnecessary log lines like:
    
    info fsevents@1.1.3: The platform "linux" is incompatible with this module.
    yarn install v1.10.1
    [1/4] Resolving packages...
    success Folder in sync.
    
    Warnings and errors are still printed.

--- ruby/entrypoint_scripts/yarn.sh
+++ ruby/entrypoint_scripts/yarn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Verifying node modules are up-to-date with yarn"
-yarn check || yarn install --check-files --mutex file
+yarn check --silent || yarn install --silent --check-files --mutex file
